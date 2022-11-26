98d2cb180fcc89ded186e77210a6b1b87beae83c
Gerard <kanetpwnz@gmail.com>
2022-11-26 17:18:48 +0100

    pseudocode

--- script.js
+++ script.js
@@ -13,22 +13,34 @@ container.forEach((div) => {
 let container = document.querySelector(`.container`);
 let newDiv = document.createElement(`div`);
 let newP = document.createElement(`p`);
+let newROW = document.newDiv.appendChild(newP);
+let startBUTTON = document.getElementById(`start`).addEventListener
+(`click`, start());
+let clearBUTTON = document.getElementById(`clear`).addEventListener
+(`click`, clear());
 
-container.newDiv.newP
+function start() {
+  clear();
+  prompt("something with arrays to take a and b");
+  // use those to run;
+  container.rows(a, b);
+}
 
-let x;
-// x = prompt(something with arrays to take a and b);
+function clear() {
+  // code to clear canvas respecting last size
+}
 
-function grid(a, b) {
+function rows(a, b) {
   if (a == b) {
     for (let i = 0; i < a; i++) {
-      newP++;
+      neWROW++;
     }
     for (let i = 0; i < b; i++) {
       newDiv++;
     }
   }
   if (a > 100 || b > 100) {
-    alert(`The number needs to be lower than 100.`)
+    alert(`The number needs to be lower than 100.`);
+    // function to restart
   }
 }
