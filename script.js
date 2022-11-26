d41ea0453bbe7ab2676000b26434502013dc5557
Gerard <kanetpwnz@gmail.com>
2022-11-26 19:17:55 +0100

    typoos

--- script.js
+++ script.js
@@ -6,32 +6,33 @@ let newDiv = document.createElement(`div`);
 newDiv.setAttribute(`class`, `gridDivs`);
 
 let newP = document.createElement(`p`);
-newP.setAttribute(`class`, `gridp`)
+newP.setAttribute(`class`, `gridp`);
 
 let NEWROW = document.container.appendChild(newDiv);
 let NEWCHILD = queryselectorAll(`.container > div`).appendChild(newP);
 
-// start button + NEED WORK
+// start button
 
 let startBUTTON = document.getElementById(`start`).addEventListener
 (`click`, start());
 
 function start() {
   clear();
-  prompt(`something with arrays to take a and b`);
-  // use those to run;
-  container.canvas(a, b);
+  var numbers = prompt("Enter two numbers separated by x: ", "");
+  let a = numbers.split("x")[0];
+  let b = numbers.split("x")[1];
+  canvas(a, b);
 }
 
 // clear button
 
 let clear = document.getElementById(`clear`);
-    clear.onclick = function(){
-     var elements = document.getElementsByClassName(`.container`);
-        for (var i = 0; i < elements.length; i++) {
+clear.onclick = function clear(){
+  var elements = document.getElementsByClassName(`.container`);
+    for (var i = 0; i < elements.length; i++) {
 
-          elements[i].removeChild();
-        }
+      elements[i].removeChild();
+    }
 }
 
 // drawing the grid function
@@ -47,7 +48,7 @@ function canvas(a, b) {
   }
   if (a > 100 || b > 100) {
     alert(`The number needs to be lower than 100.`);
-    clear++;
+    clear();
   }
 }
 
@@ -65,10 +66,10 @@ let paintp = document.getElementByClass(`gridp`).addEventListener
 
 let hooverdiv = document.getElementByClass(`gridDivs`).addEventListener
 (`hoover`, () => {
-    hooverdiv.style.backgroundColor = `gray`
+    hooverdiv.style.backgroundColor = `gray`;
   });
 
 let hooverp = document.getElementByClass(`gridp`).addEventListener
 (`hoover`, () => {
-    hooverp.style.backgroundColor = `gray`
+    hooverp.style.backgroundColor = `gray`;
   });
