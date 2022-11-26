9c9ab3507fafe0fb39fa8d3e423c0cb836d937d4
Gerard <kanetpwnz@gmail.com>
2022-11-26 17:55:19 +0100

    pseudocode

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-/* container.forEach((div) => {
-  div.addEventListener('click', () => {
-    // code
-  });
-});
-
-container.forEach((div) => {
-  div.addEventListener('hoover', () => {
-    // code
-  });
-}); */
+// declaring variables for creating elements later
 
 let container = document.querySelector(`.container`);
+
 let newDiv = document.createElement(`div`);
+newDiv.setAttribute(`class`, `gridDivs`);
+
 let newP = document.createElement(`p`);
-let newROW = document.newDiv.appendChild(newP);
+newP.setAttribute(`class`, `gridp`)
+
+let NEWROW = document.container.appendChild(newDiv);
+let NEWCHILD = queryselectorAll(`.container.div`).newP
+
+// start button & clear button functions
+
 let startBUTTON = document.getElementById(`start`).addEventListener
 (`click`, start());
 let clearBUTTON = document.getElementById(`clear`).addEventListener
@@ -23,24 +22,54 @@ function start() {
   clear();
   prompt("something with arrays to take a and b");
   // use those to run;
-  container.rows(a, b);
+  container.canvas(a, b);
 }
 
 function clear() {
   // code to clear canvas respecting last size
 }
 
-function rows(a, b) {
+// drawing the grid function
+
+function canvas(a, b) {
   if (a == b) {
     for (let i = 0; i < a; i++) {
-      neWROW++;
+      NEWCHILD++;
     }
     for (let i = 0; i < b; i++) {
-      newDiv++;
+      NEWROW++;
     }
   }
   if (a > 100 || b > 100) {
     alert(`The number needs to be lower than 100.`);
-    // function to restart
+    clear();
   }
 }
+
+/* 
+
+document.getElementByClass(`gridDivs`).addEventListener
+(`click`, () => {
+    // code to paint black
+  });
+});
+
+document.getElementByClass(`gridp`).addEventListener
+(`click`, () => {
+    // code to paint black
+  });
+});
+
+document.getElementByClass(`gridDivs`).addEventListener
+(`hoover`, () => {
+    // code trail
+  });
+});
+
+document.getElementByClass(`gridp`).addEventListener
+(`hoover`, () => {
+    // code trail
+  });
+});
+
+*/
