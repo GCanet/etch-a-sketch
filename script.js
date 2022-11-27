c96533b107dde3a04ca7357576279ece204c5ff7
Gerard <kanetpwnz@gmail.com>
2022-11-28 00:28:33 +0100

    working product

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let container = document.querySelector(`.container`);
 let startBUTTON = document.getElementById(`start`).addEventListener(`click`, startagain);
 
 function startagain() {
-  let numbers = prompt("Enter a number from 16 to 100: ", "");
+  let numbers = prompt(`Enter a number from 16 to 100: `);
   let a = numbers;
   canvas(a);
 }
@@ -13,38 +13,32 @@ function startagain() {
 // drawing the grid function
 
 function canvas(a) {
-  let total = (a * a)
-  let mod = a + 1;
-
+  let totala = a * a;
+  let squareSize = document.querySelector(`.container`).clientWidth / a;
+  
   if (a < 100 || a > 15) {
-      for (let i = 0; i < total; i++) {
-        let div = document.createElement('div');
-
-        if (i % mod === 0) {
-          div.style.cssText = "border: 1px solid black";
-        } else {
-          div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-          div.style.backgroundColor = "white"
-        }
-        container.appendChild(div).setAttribute(`class`, `gridDivs`);
-        div.innerHTML = "";
-        div.addEventListener(`click`, paintit);
-        div.addEventListener(`mouseover`, trailit);
-      }
+    for (let i = 1; i < totala; i++) {
+      let div = document.createElement(`div`);
+      container.appendChild(div).setAttribute(`class`, `gridDivs`);
+      div.style.width = squareSize + `px`;
+      div.style.height = squareSize + `px`;       
+      div.addEventListener(`click`, paintit);
+      div.addEventListener(`mouseover`, trailit);
+    }
+  }
   if (a > 100 || a < 15) {
       alert(`Number has to be lower than 100 and higher than 15.`);
-     }
   }
 }
 
-// click to black and hoover gray tail
+// click to black and hoover gray tail functions
 
 function paintit(e) {
-  e.target.style.backgroundColor = `#000`;
+  e.target.style.backgroundColor = `white`;
 }
 
 function trailit(e) {
-  e.target.style.backgroundColor = `gray`;
+  e.target.style.backgroundColor = `black`;
 }
 
 // clear button
@@ -52,7 +46,6 @@ function trailit(e) {
 let clearBUTTON = document.getElementById(`clear`).addEventListener(`click`, clearall);
 
 function clearall() {
-  while (container.firstChild) {
-      container.removeChild(container.lastChild);
+    container.innerHTML = '';
+    canvas(a);
   }
-}
